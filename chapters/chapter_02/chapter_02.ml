b4e5ff28d4441d082a8f230b5f51020cfdf75796
Mario Nebl <hello+github@mario-nebl.de>
2019-08-13 19:56:46 +0200

    implement 2.1 - 2.4

--- chapters/chapter_02/chapter_02.ml
+++ chapters/chapter_02/chapter_02.ml
@@ -39,7 +39,74 @@ let rec ldrop (Cons(_, t)) n =
     | 0 -> t ()
     | _ -> ldrop (t ()) (n - 1)
 
-(* let%test_unit _ = 
+let%test_unit _ = 
     let open OUnit2 in
-    let open Test_data in
-    assert_equal (lseq 15) (ldrop (lseq 10) 5); *)
+    assert_equal 16 (ldrop (lseq 10) 5 |> lhd) ~printer:string_of_int
+
+(* map and filter have easy analogoues in the lazy world *)
+let rec lmap f (Cons (h, tf)) = Cons (f h, fun () -> (lmap f (tf ())))
+
+let rec lfilter f (Cons (h, tf)) =
+  if f h then
+    Cons (h, fun () -> lfilter f (tf ()))
+  else
+    lfilter f (tf ())
+
+let cubes = 
+  lseq 1 
+  |> lmap (fun x -> x * x * x) 
+  |> lfilter (fun x -> x mod 5 = 0)
+
+let primes =
+  let rec mkprimes (Cons (h, tf)) =
+    Cons (h, fun () -> mkprimes (lfilter (fun x -> x mod h <> 0) (tf ())))
+  in
+  lseq 2 |> mkprimes
+
+(* There is an analogue to apped. We can combine two lists fairly, taking elements in turn from each. *)
+let rec interleave (Cons (h, tf)) l =
+  Cons (h, fun () -> interleave l (tf ()))
+
+(* The list alternating between zeros and ones can be built with interleave and a function to build constant lists *)
+let rec lconst c =
+  Cons (c, fun () -> lconst c)
+
+let zeros_ones = interleave (lconst 0) (lconst 1)
+
+(* calculate the lazy list of all ordinary lists of zeros and ones. Prepend a zero and a one to the list, interleaving the resulting lists *)
+let rec allfrom l =
+  Cons (l, fun () -> interleave (allfrom (0 :: l)) (allfrom (1 :: l)))
+
+(* 1. Write the lazy list whose elements are the numbers of 1, 2, 4, 8, 16 ... *)
+let rec mk_series n f = Cons (n, fun () -> mk_series (n * f) f)
+let quad_series = mk_series 1 2
+
+(* 2. Write a function to return the nth element of a lazy list where element zero is the head of the list *)
+let rec lnth (Cons (h, tf)) n = 
+  if n = 0 then h else lnth (tf ()) (n - 1)
+
+let%test_unit _ = 
+  let open OUnit2 in
+  assert_equal 10 (lnth (lseq 0) 10);
+  assert_equal 11 (lnth (lseq 1) 10);
+  assert_equal 2 (lnth (lseq 1) 1)
+
+(* 3. Write a function which, given a list, returns the lazy list forming a repeaded sequence taken from that list.
+   For example given the list [1; 2; 3] it should return a lazy list with elements 1, 2, 3, 1, 2, 3, 1, 2, ... *)
+let to_ring l =
+  let len = List.length l in
+  let rec to_ring' l i = Cons (List.nth l i, fun () -> to_ring' l ((i + 1) mod len)) in
+  to_ring' l 0
+
+let%test_unit _ =
+  let open OUnit2 in
+  let open Test_data in
+  assert_equal [1; 2; 3] (ltake (to_ring [1; 2; 3]) 3) ~printer:(print_list ~f:string_of_int);
+  assert_equal [1; 2; 3; 1; 2; 3; 1] (ltake (to_ring [1; 2; 3]) 7) ~printer:(print_list ~f:string_of_int);
+  assert_equal [1; 2; 3; 4; 1; 2; 3] (ltake (to_ring [1; 2; 3; 4]) 7) ~printer:(print_list ~f:string_of_int)
+
+(* 4. Write a lazy list whose elements are the fibonacci numbers 0, 1, 1, 2, 3, 5, 8 ... whose first two
+   elements are zero and one by definition, and each ensuing element is the sum of the previous two. *) 
+let fibonacci _ =
+  let rec fibonacci' m n = Cons (m + n, fun () -> fibonacci' (if m + n = 0 then 1 else n) (m + n)) in
+  fibonacci' 0 0
