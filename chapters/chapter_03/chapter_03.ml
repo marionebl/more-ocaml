aeff61a305cdef0583aca6d0dbce5c92f694f78a
Mario Nebl <hello+github@mario-nebl.de>
2019-08-16 20:57:06 +0000

    implement 2.2

--- chapters/chapter_03/chapter_03.ml
+++ chapters/chapter_03/chapter_03.ml
@@ -42,8 +42,45 @@ module Questions = struct
     (* 1. Show how to update a reference without using the := operator *)
     let update (r: 'a ref) (u: 'b): 'b ref =  r.contents <- u; r
 
-    (* 2. Using the frunctions the from the "Time Functions" section of the Unix module,
+    (* 2. Using the function the from the "Time Functions" section of the Unix module,
        write a program, which when run, returns a string containing the time and date,
        for example: "It is 2:45 on Wednesday 8 January 2014". *)
+    let rec wday = function
+        | 0 -> "Sunday"
+        | 1 -> "Monday"
+        | 2 -> "Tuesday"
+        | 3 -> "Wednesday"
+        | 4 -> "Thursday"
+        | 5 -> "Friday"
+        | 6 -> "Saturday"
+        | i -> wday (i mod 6)
+
+    let  rec month = function
+        | 0 -> "January"
+        | 1 -> "February"
+        | 2 -> "March"
+        | 3 -> "April"
+        | 4 -> "May"
+        | 5 -> "June"
+        | 6 -> "July"
+        | 7 -> "August"
+        | 8 -> "September"
+        | 9 -> "October"
+        | 10 -> "November"
+        | 11 -> "December"
+        | i -> month (i mod 6)
+
+    let announce input_time = 
+        let open Unix in
+        let open Base in
+        input_time 
+        |> Option.value ~default:(time ())
+        |> gmtime 
+        |> (fun {tm_hour; tm_min; tm_wday; tm_mday; tm_mon; tm_year; _} -> Printf.sprintf "It is %i:%i on %s, %i. %s %i" tm_hour tm_min (wday tm_wday) tm_mday (month tm_mon) (tm_year + 1900))
+
+    let%test_unit _ =
+        let open OUnit2 in
+        let open Base in
+        assert_equal "It is 20:53 on Friday, 16. August 2019" (announce (Some 1565988780.)) ~printer:Fn.id
 end
 
